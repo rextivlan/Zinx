3bb9b6ab23665b3ada84f99063156b4d2c9171cb
rextivlan <affan.ali368@gmail.com>
2021-12-19 23:58:41 +0530

    Add handlers for register and login form

--- register.php
+++ register.php
@@ -1,42 +1,6 @@
-<?php 
-function sanitizeFormUsername($inputText) {
-  $inputText = strip_tags($inputText);
-  $inputText = str_replace(" ", "", $inputText);
-  return $inputText;
-}
-
-function sanitizeFormString($inputText) {
-  $inputText = strip_tags($inputText);
-  $inputText = str_replace(" ", "", $inputText);
-  $inputText = ucfirst(strtolower($inputText));
-  return $inputText;
-}
-
-function sanitizeFormPassword($inputText) {
-  $inputText = strip_tags($inputText);
-  return $inputText;
-}
-
-  if(isset($_POST['loginButton'])) {
-    // Login Button was pressed
-  }
-
-  if(isset($_POST['registerButton'])) {
-    // Register Button was pressed
-    $username = sanitizeFormUsername($_POST['username']);
-
-    $firstName = sanitizeFormString($_POST['firstName']);
-
-    $lastName = sanitizeFormString($_POST['lastName']);
-
-    $email = sanitizeFormString($_POST['email']);
-
-    $email2 = sanitizeFormString($_POST['email2']);
-
-    $password = sanitizeFormPassword($_POST['password']);
-
-    $password2 = sanitizeFormPassword($_POST['password2']);
-  }
+<?php
+  include("includes/handlers/register-handler.php")
+  include("includes/handlers/login-handler.php")
 ?>
 
 <!DOCTYPE html>
